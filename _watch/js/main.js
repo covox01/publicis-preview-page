c2174a6519adbf2eb569968b38db8587081c1097
Dennis Oba <dennis.oba@publicisna.com>
2020-01-17 14:56:25 -0500

    Cleaned up the code

--- _watch/js/main.js
+++ _watch/js/main.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
-    function init() {
-        dropdownSelect();
-        
-    }
 
-    $.getJSON( "preview_connect.json", function( data ) {
+     $.getJSON( "preview_connect.json", function( data ) {
         console.log(data);
         
         data = data.preview_connect;
@@ -27,62 +23,60 @@ $(document).ready(function() {
         });
     }); 
 
-function dropdownSelect() {
-    var versionDropdown = document.getElementById("version-dropdown");
-    var versionOptions = document.getElementById("version-options");
-    var versionOptions
-    // var sizeDropdown = document.getElementsByClassName("size");
-    var sizeDropdown = document.getElementById("size-dropdown");
-    var sizeOptions = document.getElementById("size-options");
-    var versionDropdownClicked = false
-    var sizeDropdownClicked = false
-     // gsap.set(versionDropdown, {clip:"rect(0px, 300px, 0px, 0px);"}); 
-    
-    TweenMax.set("#version-sort-down", {rotation: 0, force3D: false})
+    function dropdownSelect() {
+    // -- Version Vars
+        var versionDropdown = document.getElementById("version-dropdown");
+        var versionOptions = document.getElementById("version-options");
+        var versionSortDown = document.getElementById("version-sort-down")
+        var versionName = document.getElementsByClassName("version-name")
+    // -- Size Vars
+        var sizeDropdown = document.getElementById("size-dropdown");
+        var sizeOptions = document.getElementById("size-options");
+        var sizeSortDown = document.getElementById("size-sort-down")
+        var sizeName = document.getElementsByClassName("size-name")
+        var versionDropdownClicked = false
+        var sizeDropdownClicked = false
+        
+        TweenMax.set(versionSortDown, {rotation: 0, force3D: false})
 
-    // Animation functions for both Version Dropdowns and Sizes Dropdowns when clicked
+        // Animation functions for both Version Dropdowns and Sizes Dropdowns when clicked
         function showVersions(){
-            gsap.set(versionOptions, {opacity: 1, y: 0})
-            $("#version-options").css("display", "block")
+            gsap.set(versionOptions, {opacity: 1, y: 0, display: "block"})
             var showVersions = new TimelineMax()
             showVersions
-                .to("#version-sort-down", {duration: .2, rotation: 180, transformOrigin: "center", opacity: .5}, "sync")
-                .to(".version", {duration: .2, borderBottomLeftRadius: "1px"}, "sync")
+                .to(versionSortDown, {duration: .2, rotation: 180, transformOrigin: "center", opacity: .5}, "sync")
+                .to(versionName, {duration: .2, borderBottomLeftRadius: "1px"}, "sync")
                 .from(versionOptions, {duration: .2, opacity: 0, y: -30}, "sync")
         }
 
         function hideVersions(){
             var hideVersion = new TimelineMax()
             hideVersion
-                .to("#version-sort-down", {duration: .2, rotation: 0, transformOrigin: "center", opacity: 1}, "sync")
-                .to(".version", {duration: .2, borderBottomLeftRadius: "5px"}, "sync")
+                .to(versionSortDown, {duration: .2, rotation: 0, transformOrigin: "center", opacity: 1}, "sync")
+                .to(versionName, {duration: .2, borderBottomLeftRadius: "5px"}, "sync")
                 .to(versionOptions, {duration: .1, ease: Power2.easeIn, opacity: 0, y: -30}, "sync")
                 .set(versionOptions, {display: "none"})
         }
 
         function showSizes(){
-            gsap.set(sizeOptions, {opacity: 1, y: 0})
-            $("#size-options").css("display", "block")
+            gsap.set(sizeOptions, {opacity: 1, y: 0, display: "block"})
             var showSizes = new TimelineMax()
             showSizes
-                .to("#size-sort-down", {duration: .2, rotation: 180, opacity: .5}, "sync")
-                .to(".size", {duration: .2, borderBottomRightRadius: "1px"}, "sync")
+                .to(sizeSortDown, {duration: .2, rotation: 180, opacity: .5}, "sync")
+                .to(sizeName, {duration: .2, borderBottomRightRadius: "1px"}, "sync")
                 .from(sizeOptions, {duration: .2, opacity: 0, y: -30}, "sync")
         }
 
         function hideSizes(){
             var hideSizes = new TimelineMax()
-        // --- Once clicked, animate arrow.
             hideSizes
-                .to("#size-sort-down", {duration: .2, rotation: 0, opacity: 1}, "sync")
-                .to(".size", {duration: .2, borderBottomRightRadius: "5px"}, "sync")
+                .to(sizeSortDown, {duration: .2, rotation: 0, opacity: 1}, "sync")
+                .to(sizeName, {duration: .2, borderBottomRightRadius: "5px"}, "sync")
                 .to(sizeOptions, {duration: .1, opacity: 0, y: -30}, "sync")
                 .set(sizeOptions, {display: "none"})
-        // --- Hide size options
-            // $("#size-options").css("display", "none")
         }
 
-    // Version options reveals if dropdown is clicked
+        // Version options reveals if dropdown is clicked
         $(versionDropdown).on('click', function() {
             event.preventDefault()
             if (!versionDropdownClicked){
@@ -91,15 +85,13 @@ function dropdownSelect() {
                 sizeDropdownClicked = false
                 showVersions()
                 hideSizes()
-
-                console.log('version dropdown', versionDropdownClicked)
             } else {
                 versionDropdownClicked = false
                 hideVersions()
             }
         });
 
-    // Size options reveals if size dropdown is clicked
+        // Size options reveals if size dropdown is clicked
         $(sizeDropdown).on('click', function() {
             event.preventDefault()
             if (!sizeDropdownClicked){
@@ -113,21 +105,21 @@ function dropdownSelect() {
             }
         });
 
-    // If user clicks outside of dropdown menu it disappears
+        // If user clicks outside of dropdown menu it disappears
         $('.trigger').on('click', function(){
             if(versionDropdownClicked === true || sizeDropdownClicked === true) {
                 versionDropdownClicked = false
                 sizeDropdownClicked = false
                 hideSizes()
                 hideVersions()
-                console.log('v-drop close', versionDropdownClicked)
             }
         })
-
+    }
+    
+    function collapseHeader(){
+        var collapseBar = document.getElementById("campaign-name-container")
         var collapse = false
-    // Click campaign name to collapse header
-        $("#campaign-name-container").on('click', function(){
-            
+        $(collapseBar).on('click', function(){
             if (!collapse) {
                 collapse = true
                 gsap.to([$("#red-banner"), $(".content")], {duration: .4, y: -120, ease: Power2.easeInOut})
@@ -135,9 +127,12 @@ function dropdownSelect() {
                 collapse = false
                 gsap.to([$("#red-banner"), $(".content")], {duration: .4, y: 0, ease: Power2.easeInOut})
             }
-            
         })
-}
+    }
 
-  init();
+    function init() {
+        dropdownSelect();
+        collapseHeader();
+    }
+    init();
 });
