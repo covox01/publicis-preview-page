dbe389afd19f6872162d2bf13d9d10bd3a01adbe
Dennis Oba <dennis.oba@publicisna.com>
2020-01-17 16:44:53 -0500

    Minor edits to the onclick on the version dropdown

--- _watch/js/main.js
+++ _watch/js/main.js
@@ -40,70 +40,79 @@ $(document).ready(function() {
         TweenMax.set(versionSortDown, {rotation: 0, force3D: false})
 
         // Animation functions for both Version Dropdowns and Sizes Dropdowns when clicked
-        function showVersions(){
-            gsap.set(versionOptions, {opacity: 1, y: 0, display: "block"})
-            var showVersions = new TimelineMax()
-            showVersions
-                .to(versionSortDown, {duration: .2, rotation: 180, transformOrigin: "center", opacity: .5}, "sync")
-                .to(versionName, {duration: .2, borderBottomLeftRadius: "1px"}, "sync")
-                .from(versionOptions, {duration: .2, opacity: 0, y: -30}, "sync")
-        }
+            function showVersions(){
+                gsap.set(versionOptions, {opacity: 1, y: 0, display: "block"})
+                var showVersions = new TimelineMax()
+                showVersions
+                    .to(versionSortDown, {duration: .2, rotation: 180, transformOrigin: "center", opacity: .5}, "sync")
+                    .to(versionName, {duration: .2, borderBottomLeftRadius: "1px"}, "sync")
+                    .from(versionOptions, {duration: .2, opacity: 0, y: -30}, "sync")
+            }
 
-        function hideVersions(){
-            var hideVersion = new TimelineMax()
-            hideVersion
-                .to(versionSortDown, {duration: .2, rotation: 0, transformOrigin: "center", opacity: 1}, "sync")
-                .to(versionName, {duration: .2, borderBottomLeftRadius: "5px"}, "sync")
-                .to(versionOptions, {duration: .1, ease: Power2.easeIn, opacity: 0, y: -30}, "sync")
-                .set(versionOptions, {display: "none"})
-        }
+            function hideVersions(){
+                var hideVersion = new TimelineMax()
+                hideVersion
+                    .to(versionSortDown, {duration: .2, rotation: 0, transformOrigin: "center", opacity: 1}, "sync")
+                    .to(versionName, {duration: .2, borderBottomLeftRadius: "5px"}, "sync")
+                    .to(versionOptions, {duration: .1, ease: Power2.easeIn, opacity: 0, y: -30}, "sync")
+                    .set(versionOptions, {display: "none"})
+            }
 
-        function showSizes(){
-            gsap.set(sizeOptions, {opacity: 1, y: 0, display: "block"})
-            var showSizes = new TimelineMax()
-            showSizes
-                .to(sizeSortDown, {duration: .2, rotation: 180, opacity: .5}, "sync")
-                .to(sizeName, {duration: .2, borderBottomRightRadius: "1px"}, "sync")
-                .from(sizeOptions, {duration: .2, opacity: 0, y: -30}, "sync")
-        }
+            function showSizes(){
+                gsap.set(sizeOptions, {opacity: 1, y: 0, display: "block"})
+                var showSizes = new TimelineMax()
+                showSizes
+                    .to(sizeSortDown, {duration: .2, rotation: 180, opacity: .5}, "sync")
+                    .to(sizeName, {duration: .2, borderBottomRightRadius: "1px"}, "sync")
+                    .from(sizeOptions, {duration: .2, opacity: 0, y: -30}, "sync")
+            }
 
-        function hideSizes(){
-            var hideSizes = new TimelineMax()
-            hideSizes
-                .to(sizeSortDown, {duration: .2, rotation: 0, opacity: 1}, "sync")
-                .to(sizeName, {duration: .2, borderBottomRightRadius: "5px"}, "sync")
-                .to(sizeOptions, {duration: .1, opacity: 0, y: -30}, "sync")
-                .set(sizeOptions, {display: "none"})
-        }
+            function hideSizes(){
+                var hideSizes = new TimelineMax()
+                hideSizes
+                    .to(sizeSortDown, {duration: .2, rotation: 0, opacity: 1}, "sync")
+                    .to(sizeName, {duration: .2, borderBottomRightRadius: "5px"}, "sync")
+                    .to(sizeOptions, {duration: .1, opacity: 0, y: -30}, "sync")
+                    .set(sizeOptions, {display: "none"})
+            }
+
+        // Update Version Name
+
 
         // Version options reveals if dropdown is clicked
-        $(versionDropdown).on('click', function() {
-            event.preventDefault()
-            if (!versionDropdownClicked){
-                $(versionDropdown).addClass("open")
-                versionDropdownClicked = true
-                sizeDropdownClicked = false
-                showVersions()
-                hideSizes()
-            } else {
-                versionDropdownClicked = false
-                hideVersions()
-            }
-        });
+            $(versionDropdown).on('click', function(e) {
+                e.preventDefault()
+
+                
+
+
+                if (!versionDropdownClicked){
+                    $(versionDropdown).addClass("open")
+                    versionDropdownClicked = true
+                    sizeDropdownClicked = false
+                    showVersions()
+                    hideSizes()
+                } else {
+                    versionDropdownClicked = false
+                    hideVersions()
+                }
+            });
 
         // Size options reveals if size dropdown is clicked
-        $(sizeDropdown).on('click', function() {
-            event.preventDefault()
-            if (!sizeDropdownClicked){
-                sizeDropdownClicked = true
-                versionDropdownClicked = false
-                showSizes()
-                hideVersions()
-            } else {
-                sizeDropdownClicked = false
-                hideSizes()
-            }
-        });
+            $(sizeDropdown).on('click', function(e) {
+                e.preventDefault()
+                if (!sizeDropdownClicked){
+                    sizeDropdownClicked = true
+                    versionDropdownClicked = false
+                    showSizes()
+                    hideVersions()
+                } else {
+                    sizeDropdownClicked = false
+                    hideSizes()
+                }
+            });
+
+        
 
         // If user clicks outside of dropdown menu it disappears
         $('.trigger').on('click', function(){
